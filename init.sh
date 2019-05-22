cb069c19119450020e13feffeca6c2bc24b8e5e9
feone <2460265235@qq.com>
2019-05-22 18:37:33 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -10,10 +10,16 @@ sed -i '' "s/InitDemo/${PROJ_NAME}/g" InitDemo.xcodeproj/project.pbxproj
 
 # 如果使用pod
 # 将project.xcworkspace 中的 contents.xcworkspacedata 中的 InitDemo 替换为 输入的项目名
-sed -i '' "s/InitDemo/${PROJ_NAME}/g" InitDemo.xcodeproj/project.xcworkspace/contents.xcworkspacedata 
+sed -i '' "s/InitDemo/${PROJ_NAME}/g" InitDemo.xcodeproj/project.xcworkspace/contents.xcworkspacedata
 
 # 修改Podfile
 sed -i '' "s/InitDemo/${PROJ_NAME}/g" Podfile
+
+
+# 重命名目录和文件名
+mv ${PWD} "$(dirname ${PWD})/${PROJ_NAME}"
+mv InitDemo.xcodeproj "${PROJ_NAME}.xcodeproj"
+mv InitDemo "${PROJ_NAME}"
 
 # 安装依赖
 pod install
