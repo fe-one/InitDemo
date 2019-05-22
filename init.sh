a71637d0e7d8e2d8fd0f3033a75496417428f39f
feone <2460265235@qq.com>
2019-05-22 15:02:47 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# 项目名
+PROJ_NAME=$1
+
+# 删除xcuserdata
+rm -rv InitDemo.xcodeproj/xcuserdata
+
+# 将project.pbxproj 中InitDemo 替换为 输入的项目名
+sed -i 's/InitDemo/${PROJ_NAME}/g' InitDemo.xcodeproj/project.pbxproj
+
+# 如果使用pod
+# 将project.xcworkspace 中的 contents.xcworkspacedata 中的 InitDemo 替换为 输入的项目名
+sed -i 's/InitDemo/${PROJ_NAME}/g' InitDemo.xcodeproj/project.xcworkspace/contents.xcworkspacedata 
+
+# 修改Podfile
+sed -i 's/InitDemo/${PROJ_NAME}/g' Podfile
+
+# 安装依赖
+pod install
+
+echo "${PROJ_NAME} init finish -.-"
